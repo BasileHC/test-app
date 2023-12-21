96dfc61c7865aada4e159944341c54f69e2fbd15
basile.raynaud <basile@homecamper.com>
2023-12-21 14:04:07 +0100

    redownloaded flutter

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/basile.raynaud/dev/flutter"
+export "FLUTTER_ROOT=/Users/basile.raynaud/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/basile.raynaud/dev_projects/home_camper_app/testapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
